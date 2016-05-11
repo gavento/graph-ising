a73ee5bd32e71830cf7c28acda5c878985e0510c
Tomas Gavenciak <gavento@ucw.cz>
2016-05-12 00:15:03 +0200

    Improvements, speedup, paralelization

--- cising.c
+++ cising.c
@@ -46,26 +46,32 @@ typedef struct {
     spin_t *spins;
     double field;
     double T;
-    index_t **neighbours; // -1 terminated neighbor list  
+    rand_t seed;
+    index_t *neigh_list; // -1 terminated neighbor list
+    index_t *neigh_offset; // for every node starting offset in neigh_list
+    index_t *degree; // node degrees
+    index_t *degree_sum; // sum of all degrees up to and incl. this node
 } ising_state;
 
 
-inline index_t ising_mc_update(ising_state *s, index_t index, rand_t *seed)
+
+
+inline index_t ising_mc_update(ising_state *s, index_t index)
 {
     index_t flipped = 0;
 
     index_t sum = 0;
-    for (index_t *p_neigh = s->neighbours[index]; *p_neigh >= 0; p_neigh++)
+    for (index_t *p_neigh = s->neigh_list + s->neigh_offset[index]; *p_neigh >= 0; p_neigh++)
         sum += s->spins[*p_neigh];
 
     double deltaE = (1 + 2 * s->spins[index]) * (s->field - sum);
     if (deltaE > 0) {
-        s->spins[index] = -s->spins[index];
+        s->spins[index] = - s->spins[index];
         flipped = 1;
     } else {
-        double p = get_rand_01(seed);
+        double p = get_rand_01(&s->seed);
         if (p < exp(deltaE / s->T)) {
-            s->spins[index] = -s->spins[index];
+            s->spins[index] = - s->spins[index];
             flipped = 1;
         }
     }
@@ -73,78 +79,81 @@ inline index_t ising_mc_update(ising_state *s, index_t index, rand_t *seed)
 }
 
 
-index_t ising_mc_sweep(ising_state *s, rand_t *seed)
+index_t ising_mc_sweep(ising_state *s)
 {
     index_t flipped = 0;
 
     index_t *perm = alloca(sizeof(index_t[s->n]));
-    rand_perm(s->n, perm, seed);
+    rand_perm(s->n, perm, &s->seed);
     for (index_t i = 0; i < s->n; i++) {
-        flipped += ising_mc_update(s, perm[i], seed);
+        flipped += ising_mc_update(s, perm[i]);
     }
 
     return flipped;
 }
 
-index_t ising_max_cluster_visit(ising_state *s, index_t index, uint8_t *visited, spin_t value, double edge_prob, rand_t *seed)
+index_t ising_max_cluster_visit(ising_state *s, index_t index, uint8_t *visited, spin_t value, double edge_prob)
 {
     index_t size = 1;
     visited[index] = 1;
-    for (index_t *p_neigh = s->neighbours[index]; *p_neigh >= 0; p_neigh++) {
+    for (index_t *p_neigh = s->neigh_list + s->neigh_offset[index]; *p_neigh >= 0; p_neigh++) {
         index_t j = *p_neigh;
-        if ((s->spins[j] == value) && (!visited[j]) && (get_rand_01(seed) < edge_prob))
-            size += ising_max_cluster_visit(s, j, visited, value, edge_prob, seed);
+        if ((s->spins[j] == value) && (!visited[j]) && (get_rand_01(&s->seed) < edge_prob))
+            size += ising_max_cluster_visit(s, j, visited, value, edge_prob);
     }
     return size;
 }
 
-index_t ising_max_cluster(ising_state *s, spin_t value, double edge_prob, rand_t *seed)
+index_t ising_max_cluster(ising_state *s, spin_t value, double edge_prob)
 {
     uint8_t *visited = memset(alloca(sizeof(int[s->n])), 0, sizeof(int[s->n]));
     index_t max_size = 0;
 
     for (index_t i = 0; i < s->n; i++) {
         if ((!visited[i]) && (s->spins[i] == value)) {
-            index_t size = ising_max_cluster_visit(s, i, visited, value, edge_prob, seed);
+            index_t size = ising_max_cluster_visit(s, i, visited, value, edge_prob);
             max_size = MAX(size, max_size);
         }
     }
     return max_size;
 }
 
+index_t ising_sweep_and_max_cluster(ising_state *s, uint32_t sweeps, spin_t value, double edge_prob)
+{
+    for (int i = 0; i < sweeps; i++)
+        ising_mc_sweep(s);
+    return ising_max_cluster(s, value, edge_prob);
+}
+
 int main_test()
 {
-    const int n = 30000;
-    spin_t spins[n];
-    index_t *neighbours[n];
-    index_t ndata[30 * n], *nn = ndata;
-    rand_t mc_seed = 42;
-    rand_t cluster_seed = 43;
-
-    for(index_t u = 0; u < n; u++) {
-        spins[u] = 1;
-        neighbours[u] = nn;
-        for (int d = -10; d <= 10; d++) {
-            if (d != 0) {
-                *nn = (u + d + n) % n;
-                nn++;
-            }
-        }
-        *nn = -1;
-        nn++;
-    }
+    const int n = 10000;
 
     ising_state s = {
         .n = n,
-        .spins = spins,
+        .spins = malloc(sizeof(spin_t[n])),
         .field = 3,
         .T = 15.,
-        .neighbours = neighbours,
+        .seed = 42,
+        .neigh_list = malloc(sizeof(index_t[30 * n])),
+        .neigh_offset = malloc(sizeof(index_t[n])),
     };
 
-    for (int i = 0; i < 2000; i++) {
-        index_t flips = ising_mc_sweep(&s, &mc_seed);
-        index_t csize = ising_max_cluster(&s, -1, 1, &cluster_seed);
+    uint32_t nlpos = 0;
+    for(index_t i = 0; i < n; i++) {
+        s.spins[i] = 1;
+        s.neigh_offset[i] = nlpos;
+        for (int d = -10; d <= 10; d++) {
+            if (d != 0) {
+                s.neigh_list[nlpos++] = (i + d + n) % n;
+            }
+        }
+        s.neigh_list[nlpos++] = -1;
+    }
+
+    for (int i = 0; i < 1000; i++) {
+        index_t flips = ising_mc_sweep(&s);
+//        index_t csize = ising_max_cluster(&s, -1, 1, &cluster_seed);
         //printf("Sweep: %d   flips: %d   max size: %d\n", i, flips, csize);
     }
     return 0;
--- ising.py
+++ ising.py
@@ -1,26 +1,175 @@
 from cffi import FFI
+import numpy as np
+import networkx as nx
 
-ffi = FFI()
-ffi.cdef("""
-    typedef uint64_t rand_t;
-    typedef int32_t index_t;
-    typedef int8_t spin_t;
+cising_cached = None
+ffi_cached = None
 
-    typedef struct {
-	index_t n;
-	spin_t *spins;
-	double field;
-	double T;
-	index_t **neighbours; // -1 terminated neighbor list  
-    } ising_state;
 
-    uint32_t get_rand(rand_t *seed);
-    double get_rand_01(rand_t *seed);
-    void rand_perm(size_t n, index_t *result, rand_t *seed);
+def cising():
 
-    index_t ising_mc_sweep(ising_state *s, rand_t *seed);
-    index_t ising_max_cluster(ising_state *s, spin_t value, double edge_prob, rand_t *seed);
-    """)
-cising = ffi.dlopen('./cising.so')
+    if not cising_cached:
+        load_ffi()
+    return cising_cached
 
+
+def ffi():
+
+    if not ffi_cached:
+        load_ffi()
+    return ffi_cached
+    
+
+def load_ffi():
+
+    global cising_cached, ffi_cached
+
+    ffi_cached = FFI()
+    ffi_cached.cdef("""
+        typedef uint64_t rand_t;
+        typedef int32_t index_t;
+        typedef int8_t spin_t;
+
+        typedef struct {
+            index_t n;
+            spin_t *spins;
+            double field;
+            double T;
+            rand_t seed;
+            index_t *neigh_list; // -1 terminated neighbor list
+            index_t *neigh_offset; // for every node starting offset in neigh_list
+            index_t *degree; // node degrees
+            index_t *degree_sum; // sum of all degrees up to and incl. this node
+        } ising_state;
+
+        uint32_t get_rand(rand_t *seed);
+        double get_rand_01(rand_t *seed);
+        void rand_perm(size_t n, index_t *result, rand_t *seed);
+
+        index_t ising_mc_sweep(ising_state *s);
+        index_t ising_max_cluster(ising_state *s, spin_t value, double edge_prob);
+        index_t ising_sweep_and_max_cluster(ising_state *s, uint32_t sweeps, spin_t value, double edge_prob);
+        """)
+
+    cising_cached = ffi_cached.dlopen('./cising.so')
+
+
+class IsingState(object):
+
+    def __init__(self, graph, spins=None, seed=42, field=0.0, T=1.0, defer_init=False):
+
+#        self.graph = nx.MultiGraph(graph)
+        self.graph = graph
+        if spins is None:
+            self.spins = np.array([1] * self.graph.order(), dtype='int8')
+        else:
+            self.spins = np.array(spins, dtype='int8')
+        self.n = self.spins.shape[0]
+        self.field = field
+        self.T = T
+        self.seed = seed
+
+        assert self.spins.dtype.name == 'int8'
+        assert self.spins.shape[0] == self.graph.order()
+        assert len(self.spins.shape) == 1
+        assert set(self.graph.nodes()) == set(range(len(self.graph.nodes())))
+
+        self.neigh_list = None
+        self.neigh_offset = None
+        self.degree = None
+        self.degree_sum = None
+
+        if not defer_init:
+            self.graph_to_internal()
+
+    def copy(self):
+
+        return IsingState(self.spins, self.graph, self.seed, self.field, self.T)
+
+    def graph_to_internal(self):
+        "Call this whenever you modify self.graph, self.field or self.T before a call to any mc_*()"
+
+        self.neigh_list = np.ndarray([2 * self.graph.size() + self.graph.order()], dtype='int32')
+        self.neigh_offset = np.ndarray([self.graph.order()], dtype='int32')
+        self.degree = np.ndarray([self.graph.order()], dtype='int32')
+        self.degree_sum = np.ndarray([self.graph.order()], dtype='int32')
+
+        offset = 0
+        for i in sorted(self.graph.nodes()):
+            self.neigh_offset[i] = offset
+            self.degree[i] = self.graph.degree(i)
+            self.degree_sum[i] = self.degree[i] + (0 if i == 0 else self.degree_sum[i - 1])
+            for j in self.graph.neighbors(i):
+                self.neigh_list[offset] = j
+                offset += 1
+            self.neigh_list[offset] = -1
+            offset += 1
+
+    def internal_to_graph(self):
+        "Call this after any graph-modifying mc_* to update self.graph"
+
+        self.graph = nx.empty_graph(self.n, create_using=nx.MultiGraph())
+        for u in sorted(self.graph.nodes()):
+            j = self.neigh_offset[u]
+            while self.neigh_list[j] >= 0:
+                v = self.neigh_list[j]
+                if u < v:
+                    self.graph.add_edge(u, v)
+                j += 1
+            
+    def prepare_state(self):
+
+        state = ffi().new("ising_state *", {
+            "n": self.n,
+            "field": self.field,
+            "T": self.T,
+            "seed": self.seed,
+            "spins": ffi().cast("int8_t *", self.spins.ctypes.data),
+            "neigh_list": ffi().cast("int32_t *", self.neigh_list.ctypes.data),
+            "neigh_offset": ffi().cast("int32_t *", self.neigh_offset.ctypes.data),
+            "degree": ffi().cast("int32_t *", self.degree.ctypes.data),
+            "degree_sum": ffi().cast("int32_t *", self.degree_sum.ctypes.data),
+            })
+        return state
+
+    def mc_sweep(self):
+
+        assert self.neigh_list is not None
+
+        state = self.prepare_state()
+        r = cising().ising_mc_sweep(state)
+        self.seed = state.seed
+        return r
+
+    def max_cluster(self, value=-1, edge_prob=1.0):
+
+        assert self.neigh_list is not None
+
+        state = self.prepare_state()
+        r = cising().ising_max_cluster(state, value, edge_prob)
+        self.seed = state.seed
+        return r
+
+    def mc_sweep_and_max_cluster(self, sweeps=10, value=-1, edge_prob=1.0):
+
+        assert self.neigh_list is not None
+
+        state = self.prepare_state()
+        r = cising().ising_sweep_and_max_cluster(state, sweeps, value, edge_prob)
+        self.seed = state.seed
+        return r
+
+
+def test():
+    n = 1000
+    spins = np.array([1] * n, dtype='int8')
+    #g = nx.grid_graph(dim=[30,30], periodic=True)
+    g = nx.random_graphs.powerlaw_cluster_graph(n, 10, 0.1, 42)
+    print(g.size(), g.order())
+
+    for i in range(1000):
+        s = IsingState(spins, g, 1.5, 3.0)
+
+    print(s.mc_sweep())
+    print(s.max_cluster(-1, 1.0))
 
